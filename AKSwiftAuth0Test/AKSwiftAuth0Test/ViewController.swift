eee730682f2392c4210d0b66e30b2e0affc14602
Iuliia Zhelem <iuliia.zhelem@akvelon.com>
2016-08-04 11:28:41 +0300

    add Email example for Swift

---
+++ AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  AKSwiftAuth0Test
+//
+//  Created by Iuliia Zhelem on 26.07.16.
+//  Copyright © 2016 Akvelon. All rights reserved.
+//
+
+import UIKit
+import Lock
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var userIdLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBAction func clickEmailWithMagicLinkButton(sender: AnyObject) {
+        self.authenticatiWithEmail(true)
+    }
+
+    @IBAction func clickEmailWithCodeButton(sender: AnyObject) {
+        self.authenticatiWithEmail(false)
+    }
+    
+    func authenticatiWithEmail(isMagicLink:Bool) {
+        let lock = A0Lock.sharedLock()
+        let controller = lock.newEmailViewController()
+        controller.useMagicLink = isMagicLink
+        controller.closable = true
+        controller.onAuthenticationBlock = {(profile: A0UserProfile!, token: A0Token!) -> () in
+            print("profile : \(profile)")
+            dispatch_async(dispatch_get_main_queue()) {
+                self.userNameLabel.text = profile.userId
+                self.userIdLabel.text = profile.email
+            }
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        lock.presentEmailController(controller, fromController: self)
+    }
+}
+
