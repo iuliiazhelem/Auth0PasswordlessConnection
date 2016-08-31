2d54d34bcc2d0818a8d8940689293d7d7f5148c6
Iuliia Zhelem <iuliia.zhelem@akvelon.com>
2016-09-01 00:14:53 +0300

    add entitlements

--- AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
+++ AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
@@ -10,9 +10,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.recieveMagicLink(_:)), name: A0LockNotificationUniversalLinkReceived, object: nil)
     }
-
+    
+    func recieveMagicLink(notification:NSNotification) {
+        print("notification : \(notification.userInfo)")
+        if let actualUserInfo = notification.userInfo, let link = actualUserInfo["com.auth0.universal-link.url"], let code = link.query {
+            print("LINK : \(link)")
+            var codeValue = code!
+            let range = codeValue.startIndex..<codeValue.startIndex.advancedBy(5)
+            codeValue.removeRange(range)
+            //You can use this code for login with email and passcode
+            //A0Lock.sharedLock().apiClient().loginWithEmail(<email>, passcode: codeValue, parameters: nil, 
+            //success: { (profile: A0UserProfile!, token: A0Token!)in
+            //    print("profile : \(profile)")
+            //    }, failure: { (error) in
+            //        print("error : \(error)")
+            //})
+            print("CODE : \(codeValue)")
+         }
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
     @IBOutlet weak var userIdLabel: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
     
@@ -40,4 +62,3 @@ class ViewController: UIViewController {
         lock.presentEmailController(controller, fromController: self)
     }
 }
-
