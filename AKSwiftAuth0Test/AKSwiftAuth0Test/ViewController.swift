4f6845a52c98d6900cc1ca6d449272fe18ac9cfe
Iuliia Zhelem <iuliia.zhelem@akvelon.com>
2016-09-01 00:44:13 +0300

    add custom UI for magic link

--- AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
+++ AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
@@ -14,20 +14,16 @@ class ViewController: UIViewController {
     }
     
     func recieveMagicLink(notification:NSNotification) {
-        print("notification : \(notification.userInfo)")
         if let actualUserInfo = notification.userInfo, let link = actualUserInfo["com.auth0.universal-link.url"], let code = link.query {
-            print("LINK : \(link)")
             var codeValue = code!
             let range = codeValue.startIndex..<codeValue.startIndex.advancedBy(5)
             codeValue.removeRange(range)
-            //You can use this code for login with email and passcode
-            //A0Lock.sharedLock().apiClient().loginWithEmail(<email>, passcode: codeValue, parameters: nil, 
-            //success: { (profile: A0UserProfile!, token: A0Token!)in
-            //    print("profile : \(profile)")
-            //    }, failure: { (error) in
-            //        print("error : \(error)")
-            //})
             print("CODE : \(codeValue)")
+            if (self.emailTextField.text?.characters.count < 1) {
+                self.showMessage("You need to eneter email");
+                return;
+            }
+            self.loginWithEmail(self.emailTextField.text!, code: codeValue)
          }
     }
     
@@ -35,9 +31,37 @@ class ViewController: UIViewController {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
+    //CUSTOM UI
+    @IBAction func clickSendEmailWithMagicLink(sender: AnyObject) {
+        if (self.emailTextField.text?.characters.count < 1) {
+            self.showMessage("You need to eneter email");
+            return;
+        }
+        let client = A0Lock.sharedLock().apiClient()
+        client.startPasswordlessWithMagicLinkInEmail(self.emailTextField.text!, parameters: A0AuthParameters.newDefaultParams(), success: {
+            self.showMessage("Please check email \(self.emailTextField.text!)")
+        }) { (error) in
+            print("error : \(error)")
+        }
+    }
+    
+    func loginWithEmail(email:String, code: String) {
+        A0Lock.sharedLock().apiClient().loginWithEmail(email, passcode: code, parameters: nil, success: { (profile: A0UserProfile!, token: A0Token!)in
+            print("profile : \(profile)")
+            dispatch_async(dispatch_get_main_queue()) {
+                self.userNameLabel.text = profile.userId
+                self.userIdLabel.text = profile.email
+            }
+            }, failure: { (error) in
+                print("error : \(error)")
+        })
+    }
+    
     @IBOutlet weak var userIdLabel: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
     
+    //LOCK UI
     @IBAction func clickEmailWithMagicLinkButton(sender: AnyObject) {
         self.authenticatiWithEmail(true)
     }
@@ -60,5 +84,13 @@ class ViewController: UIViewController {
             self.dismissViewControllerAnimated(true, completion: nil)
         }
         lock.presentEmailController(controller, fromController: self)
+    }
+    
+    func showMessage(message: String) {
+        dispatch_async(dispatch_get_main_queue()) {
+            let alert = UIAlertController(title: "Auth0", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
 }
